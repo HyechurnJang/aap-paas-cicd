b6f6a6dc285a783e9e6586bd58338c4adc0b997a
jangh <jangh@211.105.0.214>
2022-10-27 17:48:06 +0900

    update

--- src/ci.sh
+++
@@ -1,6 +0,0 @@
-# Build Container
-docker build -t 172.16.1.35/tanzu-kubernetes-clusters/vmk-nginx:$TAG_VER .
-
-# Push Container
-docker push 172.16.1.35/tanzu-kubernetes-clusters/vmk-nginx:$TAG_VER
-docker rmi -f 172.16.1.35/tanzu-kubernetes-clusters/vmk-nginx:$TAG_VER
