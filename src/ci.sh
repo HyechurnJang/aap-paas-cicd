8b15113ecfa83d4584c087ef275a1aca2f543d5c
jangh <jangh@211.105.0.214>
2022-10-27 17:41:17 +0900

    update

--- src/ci.sh
+++ src/ci.sh
@@ -1,12 +1,3 @@
-# Create Version Tag
-TAG_VER=$(date +"%Y%m%d.%H%M%S")
-echo "$TAG_VER" | tr -d '\r' > $SCRIPT_RESPONSE_FILE
-
-# Git Pull
-cd /opt/aap-paas-cicd
-git pull
-cd /opt/aap-paas-cicd/src
-
 # Build Container
 docker build -t 172.16.1.35/tanzu-kubernetes-clusters/vmk-nginx:$TAG_VER .
 
